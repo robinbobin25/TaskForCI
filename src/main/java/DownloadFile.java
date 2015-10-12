01ec8c2ce24be547373dd085ec2bbebbc5f9c618
robinbobin25 <antonina_mykhailenko@epam.com>
2015-10-12 16:23:19 +0300

    initial

---
+++ src/main/java/DownloadFile.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+/**
+ * Created by antonina_mykhailenko on 12.10.2015.
+ */
+public class DownloadFile {
+    public static void downloadFileFromURL(String urlString, File destination) {
+        try {
+            URL website = new URL(urlString);
+            ReadableByteChannel rbc;
+            rbc = Channels.newChannel(website.openStream());
+            FileOutputStream fos = new FileOutputStream(destination);
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+            fos.close();
+            rbc.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        downloadFileFromURL("https://dl.dropboxusercontent.com/u/98396761/NewTxt.txt", new File("NewTxt.txt"));
+    }
+}
